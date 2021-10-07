34e9953463eed86d980eff97916b382a9fc8023e
SYX9527 <1275241305@qq.com>
2021-10-07 15:42:51 +0800

    update

--- myTypeidea/myTypeidea/settings.py
+++ myTypeidea/myTypeidea/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 #
 # )
 # print(STATICFILES_DIRS)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_files/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
